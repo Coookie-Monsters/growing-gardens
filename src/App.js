6fc19f0a385b55b642e54bfa8e2632e04da93e6c
David Merrick <david@david-merrick.com>
2019-02-16 17:25:23 -0800

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 class App extends Component {
@@ -7,17 +7,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.growing-gardens.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            <img src={logo} className="App-logo" alt="logo" />
+            <br />
+            Growing Gardens
           </a>
         </header>
       </div>
